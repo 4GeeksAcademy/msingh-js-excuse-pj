44722462402cd5006b64c8b152ccec7465afc238
monalisasingh87 <monalisa.singh87@gmail.com>
2024-06-28 13:48:58 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import "./index.html";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function genrateRandomExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
 
+  const randomwho = who[Math.floor(Math.random() * who.length)];
+  const randomaction = action[Math.floor(Math.random() * action.length)];
+  const randomwhat = what[Math.floor(Math.random() * what.length)];
+  const randomwhen = when[Math.floor(Math.random() * when.length)];
+  const excuse = `${randomwho} ${randomaction} ${randomwhat} ${randomwhen}`;
+  return excuse;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const button = document.querySelector("#excusebutton");
+  const p = document.querySelector("#excuse");
+  button.onclick = () => {
+    p.innerHTML = genrateRandomExcuse();
+  };
 };
